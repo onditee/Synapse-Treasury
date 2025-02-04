a2c8c37dbbdb6d945452d76a6aba50fb9d03c5c1
Teddy Onditi <TeddyOtieno.Onditi@student.csulb.edu>
2025-02-04 15:45:27 -0800

    Update Treasury.sol
    Improve logic of tokenBalances when interacting with Aave

--- contracts/Treasury.sol
+++ contracts/Treasury.sol
@@ -7,6 +7,7 @@ import "c:/Users/Ted/node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol
 interface IAavePool {
     function deposit(address asset, uint256 amount, address onBehalfOf, uint16 referralCode) external;
     function withdraw(address asset, uint256 amount, address to) external;
+    function getReserveData(address asset) external view returns (uint256, address);
 }
 
 contract Treasury is ReentrancyGuard {
@@ -16,6 +17,9 @@ contract Treasury is ReentrancyGuard {
     
     mapping(address => uint256) public tokenBalances;
     mapping(address => mapping(address => uint256)) public aaveDeposits; // [user][token] => amount
+    mapping(address => uint256) public aavePrincipal; // Tracks principal deposited to Aave per token
+    mapping(address => uint256) public yieldGenerated; // Tracks interest earned per token
+    mapping(address => address) public aTokenMapping; // Maps underlying tokens to their aToken addresses
 
     IAavePool public constant AAVE_POOL = IAavePool(0x794a61358D6845594F94dc1DB02A252b5b4814aD);
     
@@ -24,6 +28,7 @@ contract Treasury is ReentrancyGuard {
     event FundsWithdrawn(address indexed token, uint256 amount);
     event DefiDeposit(address indexed token, uint256 amount);
     event DefiWithdrawal(address indexed token, uint256 amount);
+    event YieldEarned(address token, uint256 amount);
 
     // MODIFIERS
     modifier onlyOwner() {
@@ -62,14 +67,33 @@ contract Treasury is ReentrancyGuard {
         tokenBalances[_token] -= _amount;
     }
 
+
+
     // DEFI INTEGRATIONS
     function depositToAave(
         address _token,
         uint256 _amount
     ) external onlyProposals nonReentrant {
+        require(
+            IERC20(_token).balanceOf(address(this)) >= _amount,
+            "Insufficient token balance"
+        );
+
+        // Update token balance before transfer to Aave
+        tokenBalances[_token] -= _amount;
+        
+        // Get aToken address if not already mapped
+        if (aTokenMapping[_token] == address(0)) {
+            (, address aToken) = AAVE_POOL.getReserveData(_token);
+            aTokenMapping[_token] = aToken;
+        }
         IERC20(_token).approve(address(AAVE_POOL), _amount);
         AAVE_POOL.deposit(_token, _amount, address(this), 0);
         aaveDeposits[address(this)][_token] += _amount;
+        
+        // Track principal deposited
+        aavePrincipal[_token] += _amount;
+        
         emit DefiDeposit(_token, _amount);
     }
 
@@ -80,6 +104,12 @@ contract Treasury is ReentrancyGuard {
         AAVE_POOL.withdraw(_token, _amount, address(this));
         aaveDeposits[address(this)][_token] -= _amount;
         emit DefiWithdrawal(_token, _amount);
+    }
+
+    //Yeild tracking 
+    function trackYield() external {
+        uint256 yield = aaveDeposits[token] - initialDeposit;
+        emit YieldEarned(token, yield);
     }
 
     // ADMIN FUNCTIONS
