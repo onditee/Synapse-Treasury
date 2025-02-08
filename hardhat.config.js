c2284bc7b716d8529812d2c210e6409942b06dd5
Teddy Onditi <TeddyOtieno.Onditi@student.csulb.edu>
2025-02-07 21:59:32 -0800

    Debugs

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,25 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+
 module.exports = {
   solidity: {
     version: "0.8.20",
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200
+        runs: 200,
       },
-      viaIR: true
-    }
-  }
+      viaIR: true,
+    },
+  },
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "https://rpc.sepolia.dev",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 11155111,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
